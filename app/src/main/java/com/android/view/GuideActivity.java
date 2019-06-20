0b74fe6d34e55da6d9e33c8bc29cf0ca3fce80f0
hijackthe2 <2948278083@qq.com>
2019-06-20 14:49:44 +0800

    add new package named view create GuideActivity

---
+++ app/src/main/java/com/android/view/GuideActivity.java
@@ -0,0 +1,16 @@
+package com.android.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.serenityapp.R;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
