8312d9dcf122574ba3323587b8f0454ea82323ea
hijackthe2 <2948278083@qq.com>
2019-06-27 09:06:48 +0800

    增加歌词界面

---
+++ app/src/main/java/com/serenity/view/play/PlayActivity.java
@@ -0,0 +1,16 @@
+package com.serenity.view.play;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.serenityapp.R;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
--- app/src/main/java/com/serenity/view/widget/BackTitleView.java
+++ app/src/main/java/com/serenity/view/widget/BackTitleView.java
@@ -3,6 +3,7 @@ package com.serenity.view.widget;
 import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
