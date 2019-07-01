140c94d955a87a802485a2fd0560f800ac687a43
hijackthe2 <2948278083@qq.com>
2019-07-01 14:45:18 +0800

    搜索界面

--- app/src/main/java/com/serenity/view/playlist/SearchActivity.java
+++ app/src/main/java/com/serenity/view/playlist/SearchActivity.java
@@ -1,7 +1,9 @@
 package com.serenity.view.playlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,11 +11,18 @@ import android.widget.ImageView;
 import android.widget.SearchView;
 
 import com.android.serenityapp.R;
+import com.serenity.model.Song;
+import com.serenity.view.play.PlayActivity;
+
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
     private Button backBtn;
     private SearchView searchView;
+    private RecyclerView recyclerView;
+    private List<Song> songList;
+    private  SongAdapter songAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +51,24 @@ public class SearchActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        recyclerView = findViewById(R.id.search_recycler_view);
+        initSongList();
+        songAdapter = new SongAdapter(songList);
+        recyclerView.setAdapter(songAdapter);
+        songAdapter.setOnItemClickListener(new SongAdapter.OnItemClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                songAdapter.setPosition(position);
+                Intent intent  = new Intent(SearchActivity.this, PlayActivity.class);
+                //send some data to PlayActivity by using intent
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initSongList(){
+        //search result
+        songList = null;
     }
 }
